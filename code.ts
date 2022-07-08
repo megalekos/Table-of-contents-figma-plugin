ac67793889c11745809d2eb7f400ed74f200a739
megalekos <53824255+megalekos@users.noreply.github.com>
2022-07-08 21:11:29 +0200

    Added display of sections in ui

--- code.ts
+++ code.ts
@@ -1,6 +1,3 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many shapes and connectors on the screen.
-
 // This file holds the main code for the plugins. It has access to the *document*.
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (see documentation).
